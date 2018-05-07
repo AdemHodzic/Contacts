418df6f2e5d1a89bb97eccfbe1494d8f9f447841
Mona Lisa <athos.blade@gmail.com>
2018-05-08 00:08:32 +0200

    testing branch

--- src/main/java/com/adem/App/App.java
+++ src/main/java/com/adem/App/App.java
@@ -6,5 +6,7 @@ public class App {
     public static void main(String[] args) {
         Controller controller = new Controller();
         controller.start();
+        System.out.println("Hello world");
+        System.out.println("Rhy do dis");
     }
 }
